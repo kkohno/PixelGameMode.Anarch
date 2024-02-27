a3ecbbb2fcf6f42006dea2367ba36f4086d50443
SobakaBarabaka74 <153353273+SobakaBarabaka74@users.noreply.github.com>
2024-02-27 20:58:06 +0700

    Add files via upload

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,1 +1,20 @@
+// Anarchic.
+// License: © Mak.
 
+
+
+import {
+    Teams, Build, Spawns, BuildBlocksSet
+} from 'pixel_combats/room'
+import { Color } from 'pixel_combats/basic'
+
+// Создание команд
+Teams.Add('Blue', 'Teams/Blue', new Color(0, 0, 1, 0))
+var blueTeam = Teams.Get('Blue')
+blueTeam.Spawns.SpawnPointsGroups.Add(1)
+blueTeam.Build.BlocksSet.Value = BuildBlocksSet.Blue
+
+// Обработчик выбора команды
+Teams.OnRequestJoinTeam.Add((p, t) => {
+    t.add(p)
+})
